cb9460fa7bf10e4254993c7ae13a5302eb66992f
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-08 14:47:02 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400);
+  });
+})
 app.listen(3000, () => {
   console.log('App started in port 3000');
 });
